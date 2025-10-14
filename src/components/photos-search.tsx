7e67d006eb22e3cbde53fde4150ce39d6df6242a
Moacyr Barata <moacyrbarata@gmail.com>
2025-10-14 16:09:51 -0400

    refatora MainHeader para condicionalmente renderizar PhotosSearch; atualiza usePhotos para gerenciar o estado de busca

--- src/components/photos-search.tsx
+++ src/components/photos-search.tsx
@@ -2,13 +2,15 @@ import { InputText } from "./input-text";
 import SearchIcon from "../assets/icons/search.svg?react";
 import React from "react";
 import { debounce } from "../helpers/utils";
+import { usePhotos } from "../contexts/photos/hooks/use-photos";
 
 export function PhotosSearch() {
   const [inputValue, setInputValue] = React.useState("");
+  const { filter } = usePhotos();
 
   const debouncedSetValue = React.useCallback(
-    debounce((value: string) => console.log("Valor buscado:", value), 1000),
-    []
+    debounce((value: string) => filter.setQ(value), 1000),
+    [filter.setQ]
   );
 
   function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
